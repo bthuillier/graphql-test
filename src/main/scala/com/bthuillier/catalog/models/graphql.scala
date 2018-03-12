f129c63b4466c6a876441f8f02df2c05cd49e868
Benjamin Thuillier <benjaminthuillier@gmail.com>
2018-03-12 15:57:59 +0100

    refactorize

--- src/main/scala/com/bthuillier/catalog/models/graphql.scala
+++ src/main/scala/com/bthuillier/catalog/models/graphql.scala
@@ -9,64 +9,73 @@ import scala.concurrent.Future
 
 object graphql {
 
-  val artists: Fetcher[MusicCatalogService, Artist, Artist, String] =
-    Fetcher((ctx: MusicCatalogService, ids: Seq[String]) ⇒
-      Future.successful(ids.map(ctx.getArtist).map(_.get))
-    )(HasId(_.id))
+  object fetcher {
+    val artists: Fetcher[MusicCatalogService, Artist, Artist, String] =
+      Fetcher((ctx: MusicCatalogService, ids: Seq[String]) ⇒
+        Future.successful(ids.map(ctx.getArtist).map(_.get))
+      )(HasId(_.id))
 
-  val albums: Fetcher[MusicCatalogService, Album, Album, String] =
-    Fetcher((ctx: MusicCatalogService, ids: Seq[String]) ⇒
-      Future.successful(ids.map(ctx.getAlbum).map(_.get))
-    )(HasId(_.id))
+    val albums: Fetcher[MusicCatalogService, Album, Album, String] =
+      Fetcher((ctx: MusicCatalogService, ids: Seq[String]) ⇒
+        Future.successful(ids.map(ctx.getAlbum).map(_.get))
+      )(HasId(_.id))
+  }
+
+  object models {
+    implicit val ArtistType: ObjectType[Unit, Artist] =
+      deriveObjectType[Unit, Artist](
+        ObjectTypeDescription("an artist")
+      )
+
+    implicit val AlbumType: ObjectType[Unit, Album] =
+      deriveObjectType[Unit, Album](
+        ObjectTypeDescription("a music album"),
+        IncludeMethods("totalDuration"),
+        ReplaceField("artist", Field("artist", ArtistType, None,
+          resolve = ctx => fetcher.artists.defer(ctx.value.artist))
+        )
+      )
+
+    implicit val TrackType: ObjectType[Unit, Track] =
+      deriveObjectType[Unit, Track](
+        ObjectTypeDescription("a music track"),
+        ReplaceField("artists", Field("artists", ListType(ArtistType), None,
+          resolve = ctx => fetcher.artists.deferSeq(ctx.value.artists))
+        ),
+        ReplaceField("album", Field("album", AlbumType, None,
+          resolve = ctx => fetcher.albums.defer(ctx.value.album))
+        )
+      )
+
+  }
+
+  object arg {
+    val ArtistID = Argument("artistID", IDType, description = "id")
+    val AlbumID = Argument("albumID", IDType, description = "id")
+    val TrackID = Argument("trackID", IDType, description = "id")
+  }
 
   val resolver: DeferredResolver[MusicCatalogService] =
-    DeferredResolver.fetchers(artists)
-
-  implicit val ArtistType: ObjectType[Unit, Artist] =
-    deriveObjectType[Unit, Artist](
-      ObjectTypeDescription("an artist")
-    )
-
-  implicit val AlbumType: ObjectType[Unit, Album] =
-    deriveObjectType[Unit, Album](
-      ObjectTypeDescription("a music album"),
-      IncludeMethods("totalDuration"),
-      ReplaceField("artist", Field("artist", ArtistType, None,
-        resolve = ctx => artists.defer(ctx.value.artist))
-      )
-    )
-
-  implicit val TrackType: ObjectType[Unit, Track] =
-    deriveObjectType[Unit, Track](
-      ObjectTypeDescription("a music track"),
-      ReplaceField("artists", Field("artists", ListType(ArtistType), None,
-        resolve = ctx => artists.deferSeq(ctx.value.artists))
-      ),
-      ReplaceField("album", Field("album", AlbumType, None,
-        resolve = ctx => albums.defer(ctx.value.album))
-      )
-    )
-
-  val IDArg = Argument("id", IDType, description = "id")
+    DeferredResolver.fetchers(fetcher.artists)
 
   val Query: ObjectType[MusicCatalogService, Unit] = ObjectType[MusicCatalogService, Unit](
     "Query", fields[MusicCatalogService, Unit](
-      Field("artist", OptionType(ArtistType),
-        arguments = IDArg :: Nil,
-        resolve = ctx ⇒ ctx.ctx.getArtist(ctx.arg(IDArg))),
-      Field("album", OptionType(AlbumType),
-        arguments = IDArg :: Nil,
-        resolve = ctx ⇒ ctx.ctx.getAlbum(ctx.arg(IDArg))),
-      Field("track", OptionType(TrackType),
-        arguments = IDArg :: Nil,
-        resolve = ctx ⇒ ctx.ctx.getTrack(ctx.arg(IDArg)))
+      Field("artist", OptionType(models.ArtistType),
+        arguments = arg.ArtistID :: Nil,
+        resolve = ctx ⇒ ctx.ctx.getArtist(ctx.arg(arg.ArtistID))),
+      Field("album", OptionType(models.AlbumType),
+        arguments = arg.AlbumID :: Nil,
+        resolve = ctx ⇒ ctx.ctx.getAlbum(ctx.arg(arg.AlbumID))),
+      Field("track", OptionType(models.TrackType),
+        arguments = arg.TrackID :: Nil,
+        resolve = ctx ⇒ ctx.ctx.getTrack(ctx.arg(arg.TrackID)))
     ))
 
   val Mutation: ObjectType[MusicCatalogService, Unit] = ObjectType[MusicCatalogService, Unit](
     "Mutation", fields[MusicCatalogService, Unit](
       Field("addTrack", OptionType(StringType),
-        arguments = IDArg :: Nil,
-        resolve = ctx => ctx.ctx.addTrack(ctx.arg(IDArg))
+        arguments = arg.ArtistID :: Nil,
+        resolve = ctx => ctx.ctx.addTrack(ctx.arg(arg.ArtistID))
       )
     ))
 
